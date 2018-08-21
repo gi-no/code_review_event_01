3f9050a57c33a473fdfaa18ddbbf9fb26c427b03
yukimura1227 <takamura1227@gmail.com>
2018-08-21 21:09:25 +0900

    feat: extract challenge result summary

--- hiroyuki.takamura/main.rb
+++ hiroyuki.takamura/main.rb
@@ -1,10 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
 
-html = open('https://paiza.jp/challenges/ranks/S') do |page|
+html = open('https://paiza.jp/challenges/ranks/s/info') do |page|
   page.read
 end
 
 contents = Nokogiri::HTML.parse(html)
 
-puts contents.title
+contents.css('.problem-box').each do |element|
+  title = element.at_css('.problem-box__header__title').inner_text.strip
+  expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+  answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+  puts title
+  puts expect_time_for_answer
+  puts answer_info_summary
+end

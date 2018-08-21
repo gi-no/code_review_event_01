a3857e187343aac846cc40b51f89be9cac7f6df7
yukimura1227 <takamura1227@gmail.com>
2018-08-21 21:22:04 +0900

    feat: access all ranks(S-D)

--- hiroyuki.takamura/main.rb
+++ hiroyuki.takamura/main.rb
@@ -1,19 +1,28 @@
 require 'open-uri'
 require 'nokogiri'
 
-html = open('https://paiza.jp/challenges/ranks/s/info') do |page|
-  page.read
-end
+urls = %w[
+  https://paiza.jp/challenges/ranks/s/info
+  https://paiza.jp/challenges/ranks/a/info
+  https://paiza.jp/challenges/ranks/b/info
+  https://paiza.jp/challenges/ranks/c/info
+  https://paiza.jp/challenges/ranks/d/info
+]
 
-contents = Nokogiri::HTML.parse(html)
+urls.each do |url|
+  html = open(url) do |page|
+    page.read
+  end
 
-File.open('s.txt', 'w') do |f|
-  contents.css('.problem-box').each do |element|
-    title = element.at_css('.problem-box__header__title').inner_text.strip
-    expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
-    answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+  contents = Nokogiri::HTML.parse(html)
 
-    f.puts "#{title} #{expect_time_for_answer} #{answer_info_summary}"
+  File.open('result.txt', 'w') do |f|
+    contents.css('.problem-box').each do |element|
+      title = element.at_css('.problem-box__header__title').inner_text.strip
+      expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+      answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+
+      f.puts "#{title} #{expect_time_for_answer} #{answer_info_summary}"
+    end
   end
 end
-

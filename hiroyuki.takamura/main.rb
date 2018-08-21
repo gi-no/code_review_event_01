df77ee32d317c4ff0ca206786fc866d397ea1cf6
yukimura1227 <takamura1227@gmail.com>
2018-08-21 21:18:30 +0900

    feat: output parse data in text file

--- hiroyuki.takamura/main.rb
+++ hiroyuki.takamura/main.rb
@@ -7,11 +7,13 @@ end
 
 contents = Nokogiri::HTML.parse(html)
 
-contents.css('.problem-box').each do |element|
-  title = element.at_css('.problem-box__header__title').inner_text.strip
-  expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
-  answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
-  puts title
-  puts expect_time_for_answer
-  puts answer_info_summary
+File.open('s.txt', 'w') do |f|
+  contents.css('.problem-box').each do |element|
+    title = element.at_css('.problem-box__header__title').inner_text.strip
+    expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+    answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+
+    f.puts "#{title} #{expect_time_for_answer} #{answer_info_summary}"
+  end
 end
+

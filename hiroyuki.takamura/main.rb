9f4adac5facb2890485e064ea7389b7176a03f2e
yukimura1227 <takamura1227@gmail.com>
2018-08-22 15:25:39 +0900

    feat: output file on csv format

--- hiroyuki.takamura/main.rb
+++ hiroyuki.takamura/main.rb
@@ -19,10 +19,14 @@ urls.each do |url|
   File.open('result.txt', 'w') do |f|
     contents.css('.problem-box').each do |element|
       title = element.at_css('.problem-box__header__title').inner_text.strip
-      expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
-      answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ')
+      expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[1]
+      limit_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[3]
+      answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split
+      correct_answer_rate = answer_info_summary[1]
+      avarage_answer_time = answer_info_summary[3]
+      avarage_score       = answer_info_summary[5]
 
-      f.puts "#{title} #{expect_time_for_answer} #{answer_info_summary}"
+      f.puts %W[#{title} #{expect_time_for_answer} #{limit_time_for_answer} #{correct_answer_rate} #{avarage_answer_time} #{avarage_score}].join(',')
     end
   end
 end

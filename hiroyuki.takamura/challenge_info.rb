09952489dc29b026746052f269c52a8745a750ec
yukimura1227 <takamura1227@gmail.com>
2018-08-27 14:05:22 +0900

    refact: using regex instead of split

--- hiroyuki.takamura/challenge_info.rb
+++ hiroyuki.takamura/challenge_info.rb
@@ -5,7 +5,8 @@ class ChallengeInfo
     :expect_time_for_answer,
     :limit_time_for_answer,
     :correct_answer_rate,
-    :average_answer_time,
+    :average_answer_time_min,
+    :average_answer_time_sec,
     :average_score
   )
 
@@ -13,7 +14,7 @@ class ChallengeInfo
     self.title = prase_header_title(problem_box_element)
     self.expect_time_for_answer, self.limit_time_for_answer =
       parse_header_note(problem_box_element)
-    self.correct_answer_rate, self.average_answer_time, self.average_score =
+    self.correct_answer_rate, self.average_answer_time_min, self.average_answer_time_sec, self.average_score =
       parse_problem_box_data(problem_box_element)
   end
 
@@ -28,15 +29,16 @@ class ChallengeInfo
   def parse_header_note(problem_box_element)
     header_note_element =
       problem_box_element.at_css('.problem-box__header__note')
-    raise '.problem-box__header__noteをparseできませんでした。' unless header_note_element
-    tokens = header_note_element.inner_text.tr("\n", ' ').gsub(/\s+/, ' ').split
-    [tokens[1], tokens[3]]
+    match_data = header_note_element&.text.match(/想定回答時間\n(\d+)\n分.*制限時間\n(\d+)\n時間/)
+    raise '.problem-box__header__noteをparseできませんでした。' unless match_data
+    [match_data[1].to_i, match_data[2].to_i]
   end
 
   def parse_problem_box_data(problem_box_element)
     box_data_element = problem_box_element.at_css('.problem-box__data')
+    match_data =
+      box_data_element.text.match(/正解率：\n(\d+\.\d+)％\n平均回答時間：\n(\d+)分(\d+)秒\n平均スコア：\n+(\d+\.\d+)点/)
     raise '.problem-box__dataをparseできませんでした。' unless box_data_element
-    tokens = box_data_element.inner_text.tr("\n", ' ').gsub(/\s+/, ' ').split
-    [tokens[1], tokens[3], tokens[5]]
+    [match_data[1].to_f, match_data[2].to_i, match_data[3].to_i, match_data[4].to_f]
   end
 end

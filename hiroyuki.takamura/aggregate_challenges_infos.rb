c72543f06a7d0307761996730d0721d6d9b5ea01
yukimura1227 <takamura1227@gmail.com>
2018-08-27 11:34:10 +0900

    refact: extract setup method

--- hiroyuki.takamura/aggregate_challenges_infos.rb
+++ hiroyuki.takamura/aggregate_challenges_infos.rb
@@ -20,14 +20,7 @@ urls.each do |url|
 
   contents.css('.problem-box').each do |element|
     challenge_info = ChallengeInfo.new
-    challenge_info.title = element.at_css('.problem-box__header__title').inner_text.strip
-    challenge_info.expect_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[1]
-    challenge_info.limit_time_for_answer = element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[3]
-    answer_info_summary = element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split
-    challenge_info.correct_answer_rate = answer_info_summary[1]
-    challenge_info.average_answer_time = answer_info_summary[3]
-    challenge_info.average_score       = answer_info_summary[5]
-
+    challenge_info.setup_by_problem_box(element)
     challenge_infos << challenge_info
   end
 end
--- hiroyuki.takamura/challenge_info.rb
+++ hiroyuki.takamura/challenge_info.rb
@@ -8,4 +8,14 @@ class ChallengeInfo
     :average_answer_time,
     :average_score
   )
+
+  def setup_by_problem_box(problem_box_element)
+    self.title = problem_box_element.at_css('.problem-box__header__title').inner_text.strip
+    self.expect_time_for_answer = problem_box_element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[1]
+    self.limit_time_for_answer = problem_box_element.at_css('.problem-box__header__note').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split[3]
+    answer_info_summary = problem_box_element.at_css('.problem-box__data').inner_text.gsub(/\n/, ' ').gsub(/\s+/, ' ').split
+    self.correct_answer_rate = answer_info_summary[1]
+    self.average_answer_time = answer_info_summary[3]
+    self.average_score       = answer_info_summary[5]
+  end
 end
